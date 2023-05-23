a257fdb5b009f0c4d69885d9c9678a15373b11cd
Pedro Artur Silva Costa <2022113TADS0103@ifpi.int>
2023-05-23 16:35:55 -0300

    mudancas bonitas

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 window.addEventListener('scroll', () => {
     document.body.style.setProperty('--scroll', window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
   }, false);
+
+// add class navbarDark on navbar scroll
+const header = document.querySelector('.navbar');
+
+window.onscroll = function() {
+    var top = window.scrollY;
+    if(top >=900) {
+        header.classList.add('navbarDark');
+    }
+    else {
+        header.classList.remove('navbarDark');
+    }
+}
